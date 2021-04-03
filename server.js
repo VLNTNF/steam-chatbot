c14f4949c3339dfb2f7b3be299e2881a01771202
Valentin F <valentin.fosse.76@gmail.com>
2021-04-03 23:29:52 +0200

    Games recommendation

--- server.js
+++ server.js
@@ -19,27 +19,29 @@ server.post('/', (request, response, data) => {
     const userData = FB.messageHandler(data);
     console.log(userData.content.nlp.intents);
     console.log(userData.content.nlp.entities);
-    STEAM(userData.content.nlp, userData.sender).then(value => {
+    STEAM(userData.content.nlp, userData.sender).then(async value => {
       try {
-        if(value.txt){
-          if(Array.isArray(value.txt)){
-            value.txt.forEach(txt => {
+        if (value.txt) {
+          if (Array.isArray(value.txt)) {
+            for (let txt of value.txt) {
               FB.txt(userData.sender, txt);
-            });
+              await STEAM.sleep(200);
+            }
           } else {
             FB.txt(userData.sender, value.txt);
           }
         }
-        if(value.img){
-          if (Array.isArray(value.img)){
-            value.img.forEach(img => {
+        if (value.img) {
+          if (Array.isArray(value.img)) {
+            for (let img of value.img) {
               FB.img(userData.sender, img);
-            });
+              await STEAM.sleep(200);
+            }
           } else {
             FB.img(userData.sender, value.img);
           }
         }
-      } catch(error) {}
+      } catch (error) { }
     });
   });
 });
--- steam/index.js
+++ steam/index.js
@@ -6,6 +6,49 @@ const path = require('path');
 const filePath = path.join(__dirname, 'data.json');
 
 /**
+ * Utility
+ */
+const unixToDate = (unix) => {
+    var a = new Date(unix * 1000);
+    var months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    var time = {};
+    time.year = a.getFullYear();
+    time.month = months[a.getMonth()];
+    time.day = a.getDate();
+    time.hour = a.getHours();
+    time.min = a.getMinutes();
+    time.sec = a.getSeconds();
+    return time;
+};
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const sortDict = (dict, key) => {
+    var array = []
+    for (var id in dict) {
+        let e = dict[id];
+        e.id = id;
+        array.push(e);
+    }
+    array.sort((a, b) => {
+        return b[key] - a[key];
+    });
+    return array;
+};
+
+const maxArray = (arr, key) => {
+    var max = arr[0][key];
+    arr.forEach(x => {
+        if(x[key] > max){
+            max = x[key]
+        }
+    });
+    return max;
+};
+
+/**
  * Fetch
  */
 const fetch = async (url) => {
@@ -26,32 +69,6 @@ const facebook = async (id) => {
     let url = `https://graph.facebook.com/${id}?fields=first_name,last_name&access_token=${config.FB.ACCESS_TOKEN}`;
     const data = await fetch(url);
     return data;
-};
-
-/**
- * Steam profile
- */
-const personProfile = async (id) => {
-    let url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${config.STEAM}&steamids=${id}`;
-    const data = await fetch(url);
-    if (data) {
-        try {
-            let player = data.response.players[0];
-            let profile = {};
-            profile.name = player.personaname;
-            profile.id = player.steamid;
-            profile.image = player.avatarfull;
-            if(player.communityvisibilitystate >= 3){
-                profile.private = false;
-            } else {
-                profile.private = true;
-            }
-            return profile;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    return false;
 };
 
 /**
@@ -108,6 +125,172 @@ const findID64 = async (input) => {
 };
 
 /**
+ * Steam profile
+ */
+const personProfile = async (id) => {
+    let url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${config.STEAM}&steamids=${id}`;
+    const data = await fetch(url);
+    if (data) {
+        try {
+            let player = data.response.players[0];
+            let profile = {};
+            profile.name = player.personaname;
+            profile.id = player.steamid;
+            profile.image = player.avatarfull;
+            if (player.communityvisibilitystate >= 3) {
+                profile.private = false;
+            } else {
+                profile.private = true;
+            }
+            return profile;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return false;
+};
+
+const personFriends = async (id) => {
+    let url = `http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${config.STEAM}&steamid=${id}&relationship=friend`;
+    const data = await fetch(url);
+    if (data) {
+        try {
+            var list = [];
+            let friends = data.friendslist.friends;
+            if (friends.length > 0) {
+                friends.forEach(f => {
+                    list.push(f.steamid);
+                });
+                return list;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return false;
+};
+
+const ownedGames = async (id) => {
+    let url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${config.STEAM}&steamid=${id}&include_appinfo=true&format=json`;
+    const data = await fetch(url);
+    if (data.response) {
+        try {
+            var res = [];
+            let games = data.response.games;
+            if (games && games.length > 0) {
+                games.forEach(x => {
+                    if (x.playtime_forever > 60) {
+                        let game = {};
+                        game.id = x.appid;
+                        game.name = x.name;
+                        game.time = x.playtime_forever;
+                        res.push(game);
+                    }
+                });
+                if (res.length > 0) {
+                    return res;
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return false;
+};
+
+const likedGenres = async (id) => {
+    let games = await ownedGames(id);
+    if (games) {
+        try {
+            var info = {};
+            for (let game of games) {
+                if (game.time > 0) {
+                    let x = await gameInfo(game.id);
+                    if (x && x.genres.length > 0) {
+                        x.genres.forEach(genre => {
+                            info[genre] = (info[genre] || { count: 0, time: 0 });
+                            info[genre].count += 1;
+                            info[genre].time += game.time;
+                        });
+                    }
+                }
+            }
+            for (var g in info){
+                let genre = info[g];
+                genre.avg = parseFloat((genre.time/genre.count).toFixed(2));
+            }
+            return info;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return false;
+};
+
+const friendGames = async (id) => {
+    let friendList = await personFriends(id);
+    if (friendList) {
+        try {
+            var info = {};
+            for (let friend of friendList) {
+                let games = await ownedGames(friend);
+                if (games) {
+                    games.forEach(x => {
+                        info[x.id] = (info[x.id] || { count: 0, name: x.name, time: 0 });
+                        info[x.id].count += 1;
+                        info[x.id].time += x.time;
+                    });
+                }
+            }
+            for (var id in info){
+                let game = info[id];
+                game.avg = parseFloat((game.time/game.count).toFixed(2));
+            }
+            return info;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return false;
+};
+
+const gamesAttractivity = async (id) => {
+    var games = await friendGames(id);
+    if (games) {
+        try {
+            var sortedGames = sortDict(games, "avg");
+            sortedGames = sortedGames.slice(0, 10);
+            var maxgame = maxArray(sortedGames, "avg");
+            sortedGames.forEach(x => {
+                x.normal = x.avg/maxgame;
+            });
+            for (var game of sortedGames) {
+                let info = await gameInfo(game.id);
+                let genres = [];
+                if (info && info.genres.length > 0) {
+                    genres = info.genres;
+                }
+                game.genres = genres;
+            }
+            console.log(sortedGames);
+            var liked = await likedGenres(id);
+            if(liked){
+                var sortedGenres = sortDict(liked, "avg");
+                var maxgenre = maxArray(sortedGenres, "avg");
+                sortedGenres.forEach(x => {
+                    x.normal = x.avg/maxgenre;
+                });
+                console.log(sortedGenres);
+                return(sortedGames.slice(0,3));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return false;
+};
+
+/**
  * Game info
  */
 const searchGame = async (n) => {
@@ -126,7 +309,6 @@ const searchGame = async (n) => {
                 if (x.appid && s.includes(n)) {
                     // && !s.includes('serv') && !s.includes('sdk') && !s.includes('beta') && !s.includes('vers')
                     test = await fetch(`https://store.steampowered.com/api/appdetails?appids=${x.appid}&l=english`);
-                    console.log(`${x.appid} : ${test}`);
                     if (test[`${x.appid}`].success && test[`${x.appid}`].data.type == "game") {
                         app = x;
                         break;
@@ -146,24 +328,25 @@ const searchGame = async (n) => {
 
 const gameInfo = async (id) => {
     let url = `https://store.steampowered.com/api/appdetails?appids=${id}&l=english`;
-    console.log(url);
     const d = await fetch(url);
     let info = {};
     if (d) {
         try {
             const data = d[`${id}`].data;
-            info.name = data.name;
-            info.desc = data.short_description;
-            info.img = data.header_image;
-            info.meta = (data.metacritic) ? data.metacritic.score : null;
-            info.date = (data.release_date) ? data.release_date.date : null;
-            info.genres = [];
-            if (data.genres) {
-                data.genres.forEach(x => {
-                    info.genres.push(x.description);
-                });
+            if (data) {
+                info.name = data.name;
+                info.desc = data.short_description;
+                info.img = data.header_image;
+                info.meta = (data.metacritic) ? data.metacritic.score : null;
+                info.date = (data.release_date) ? data.release_date.date : null;
+                info.genres = [];
+                if (data.genres) {
+                    data.genres.forEach(x => {
+                        info.genres.push(x.description);
+                    });
+                }
+                return info;
             }
-            return info;
         } catch (error) {
             console.error(error);
         }
@@ -171,30 +354,30 @@ const gameInfo = async (id) => {
     return false;
 };
 
-/**
- * Games list
- */
-const ownedGames = async (id) => {
-    let url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${config.STEAM}&steamid=${id}&include_appinfo=true&format=json`;
-    const data = await fetch(url);
-    var res = [];
-    if (data.response) {
+const gameNews = async (id) => {
+    let url = `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${id}&count=3&maxlength=50&format=json`;
+    const d = await fetch(url);
+    if (d) {
         try {
-            let games = data.response.games;
-            games.forEach(x => {
-                if (x.playtime_forever > 60) {
-                    let game = {};
-                    game.id = x.appid;
-                    game.name = x.name;
-                    game.time = x.playtime_forever;
-                    res.push(game);
-                }
-            });
+            if (d.appnews.count > 0) {
+                var newsArray = [];
+                d.appnews.newsitems.forEach(data => {
+                    let news = {};
+                    news.author = data.feedlabel;
+                    news.title = data.title;
+                    news.link = data.url.replace(' ', '');
+                    news.date = data.date;
+                    let date = unixToDate(data.date);
+                    news.dateString = `${date.month} ${date.day}, ${date.year}`;
+                    newsArray.push(news);
+                });
+                return newsArray;
+            }
         } catch (error) {
             console.error(error);
         }
     }
-    return res;
+    return false;
 };
 
 /**
@@ -251,17 +434,30 @@ module.exports = (nlpData, senderID) => {
         }
         else if (ext[0] == 'news') {
             try {
-                let response = await getCredits(movie, releaseYear);
-                var directors = [];
-                response.crew.forEach(entry => {
-                    if (entry.job == 'Director') {
-                        directors.push(entry.name);
+                let appID = await searchGame(ext[1]);
+                if (appID) {
+                    let appNews = await gameNews(appID);
+                    if (appNews) {
+                        var texts = [];
+                        appNews.forEach(news => {
+                            let text = [`News from ${news.author} (${news.dateString})`];
+                            text.push(`${news.title}`);
+                            text.push(`${news.link}`);
+                            texts.push(text.join('\n\n'));
+                        });
+                        resolve({
+                            txt: texts,
+                            img: null
+                        });
+                    } else {
+                        resolve({
+                            txt: "No news for this game 😮",
+                            img: null
+                        });
                     }
-                });
-                if (response) {
-                    let text = `${response['title']} has been directed by ${directors.join(', ')}.`;
+                } else {
                     resolve({
-                        txt: text,
+                        txt: "Game not found 😕",
                         img: null
                     });
                 }
@@ -274,20 +470,27 @@ module.exports = (nlpData, senderID) => {
                     let id = json[senderID];
                     if (id) {
                         let profile = await personProfile(id);
-                        if(profile.private){
+                        if (profile.private) {
                             resolve({
-                                txt: "Your account is private!\nSwitch to public to use features",
+                                txt: "Your account is private!\nSwitch to public to use features 😄",
                                 img: null
                             });
                         } else {
-                            resolve({
-                                txt: "Suggestion mode coming soon",
-                                img: null
-                            });
+                            var games = await gamesAttractivity(id);
+                            if (games) {
+                                var texts = [];
+                                games.forEach(g => {
+                                    texts.push(`${g.name}\n\nGenres: ${g.genres.join(', ')}`)
+                                })
+                                resolve({
+                                    txt: texts,
+                                    img: null
+                                });
+                            }
                         }
                     } else {
                         resolve({
-                            txt: 'Add your steam account by answering "My steam:" + your link/ID64/username',
+                            txt: ["I don't know your steam account 😅", 'Add it by answering: My steam is _(your link/ID64/username)_'],
                             img: null
                         });
                     }
@@ -304,8 +507,8 @@ module.exports = (nlpData, senderID) => {
                     console.log(profile);
                     if (profile) {
                         let text = [`${profile.name} (${profile.id})`]
-                        if(profile.private){
-                            text.push('Your account is private!\nSwitch to public to use features')
+                        if (profile.private) {
+                            text.push('Your account is private!\nSwitch to public to use features 😄')
                         }
                         resolve({
                             txt: text,
@@ -323,7 +526,8 @@ module.exports = (nlpData, senderID) => {
             try {
                 let response = await facebook(senderID);
                 if (response) {
-                    let text = `Hello ${response.first_name} 😀`;
+                    let text = [`Hello *${response.first_name}* 👋🏻`];
+                    text.push('Ask me what you want:\ntell me...\n- games to play\n- news about _(a game)_\n- more about _(another game)_')
                     resolve({
                         txt: text,
                         img: null
@@ -342,4 +546,8 @@ module.exports = (nlpData, senderID) => {
             img: null
         });
     });
+};
+
+module.exports.sleep = (ms) => {
+    return sleep(ms);
 };
